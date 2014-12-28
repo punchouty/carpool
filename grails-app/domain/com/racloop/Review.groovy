bea08d1869056bd3015ac8425005e13e7fc160c0
Rohit Arora <rarora@gmail.com>
2014-12-28 15:45:52 +0530

    Added user rating API
    Added user rating API

--- grails-app/domain/com/racloop/Review.groovy
+++ grails-app/domain/com/racloop/Review.groovy
@@ -7,6 +7,11 @@ class Review {
 	String comment
 	boolean isReviewerADriver
 	
+	private transient final double  w_safety = 1d
+	private transient final double  w_comfort = 1d
+	private transient final double  w_punctualty = 1d
+	private transient final double  w_overall = 1d
+	
 	//for driver only
 	//double luggage
 	//double loud
@@ -24,5 +29,20 @@ class Review {
 	
 	static mapping = {
 		comment type: 'text'
+	}
+	
+	static transients = ["w_safety","w_safety","w_punctualty", "w_overall"]
+	
+	
+	public double getAverageRating() {
+
+		double weightedSum = (punctualty * w_punctualty) + (overall * w_overall)
+		double totalWeight =  w_punctualty + w_overall
+		if(!isReviewerADriver) {
+			weightedSum = weightedSum + (safety * w_safety) + (comfort * w_comfort)
+			totalWeight = totalWeight + w_safety + w_comfort
+		}
+		double reviewRating = weightedSum/totalWeight
+		return reviewRating
 	}
 }
---
+++ test/integration/com/racloop/user/UserRatingTestSpec.groovy
@@ -0,0 +1,116 @@
+package com.racloop.user
+
+
+
+import grails.plugin.nimble.InstanceGenerator
+import grails.plugin.nimble.core.UserBase
+
+import org.junit.Test
+
+import spock.lang.*
+
+import com.racloop.Review
+import com.racloop.User
+
+/**
+ *
+ */
+class UserRatingTestSpec extends Specification {
+	def grailsApplication
+    def setup() {
+		//Add Sample Driver and Sample Rider user here if they are not getting added from Bootstarp.groovy
+    }
+
+    def cleanup() {
+    }
+
+    @Test
+	void testSavingUserReviewAsRider() {
+		Review review = getSampleReview1()
+		when:
+		review.save()
+		then: 
+		review.id != null
+		review.getAverageRating() == 3.0d
+    }
+	
+	@Test
+	void testSavingUserReviewAsDriver() {
+		Review review = getSampleReview3()
+		when:
+		review.save()
+		then:
+		review.id != null
+		review.getAverageRating() == 2.5d
+	}
+	
+	@Test
+	void testGetRatingForAUser() {
+		Review review1 = getSampleReview1()
+		Review review2 = getSampleReview2()
+		Review review3 = getSampleReview3()
+		when:
+		review1.save()
+		review2.save()
+		review3.save()
+		User user = User.findByUsername("sample.rider@racloop.com")
+		then:
+		def reviewList = user.getIncomingReviews()
+		reviewList.size() == 3
+		user.getUserRating() == 2.5d
+		
+		
+	}
+	
+	@Test
+	void testGetDefaultRatingForAUser() {
+		when:
+		User user = User.findByUsername("sample.rider@racloop.com")
+		then:
+		def reviewList = user.getIncomingReviews()
+		reviewList.size() == 0
+		user.getUserRating() == 0.0d
+		
+	}
+	
+	private Review getSampleReview1(){
+		Review review = new Review()
+		review.reviewer = User.findByUsername("sample.driver@racloop.com")
+		review.reviewee = User.findByUsername("sample.rider@racloop.com")
+		review.comment = "Test comment as Rider"
+		review.safety = 3.0d
+		review.comfort = 3.0d
+		review.punctualty = 3.0d
+		review.overall = 3.0d
+		return review
+		
+	}
+	
+	private Review getSampleReview2(){
+		Review review = new Review()
+		review.reviewer = User.findByUsername("sample.driver@racloop.com")
+		review.reviewee = User.findByUsername("sample.rider@racloop.com")
+		review.comment = "Test comment as Rider"
+		review.safety = 2.0d
+		review.comfort = 2.0d
+		review.punctualty = 2.0d
+		review.overall = 2.0d
+		return review
+	
+	}
+	
+	private Review getSampleReview3(){
+		Review review = new Review()
+		review.reviewer = User.findByUsername("sample.driver@racloop.com")
+		review.reviewee = User.findByUsername("sample.rider@racloop.com")
+		review.isReviewerADriver = true
+		review.comment = "Test Comment as Driver"
+		review.safety = 2.0d
+		review.comfort = 2.0d
+		review.punctualty = 2.5d
+		review.overall = 2.5d
+		return review
+	
+	}
+}
+
